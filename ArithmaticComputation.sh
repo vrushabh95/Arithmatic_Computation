710834d74e351aec3f2fa0218280f95d1adb9625
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 16:47:32 +0530

    Computing Operation

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -4,3 +4,5 @@ echo "Welcome To Arithmatic Computation"
 read -p "Enter First value: " input1
 read -p "Enter Second value: " input2
 read -p "Enter Third value: " input3
+
+operation1=$(( input1 + input2 * input3 ))
