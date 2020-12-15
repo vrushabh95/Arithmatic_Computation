7d79f723cb0e80dcf3b5ca5ab06ad25bc32b167e
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 17:10:03 +0530

    Store The Result In Dictionary

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -9,3 +9,13 @@ operation1=$(( input1 + input2 * input3 ))
 operation2=$(( input1 * input2 + input3 ))
 operation3=$(( input3 + input1 / input2 ))
 operation4=$(( input1 % input2 + input3 ))
+
+#Storing the expressions result in a dictionary
+declare -A  results
+
+results[result1]=$operation1
+results[result2]=$operation2
+results[result3]=$operation3
+results[result4]=$operation4
+
+echo ${results[@]}
