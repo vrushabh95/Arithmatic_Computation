7f1358a36497ae4a48fcfa8844e5b49fb478bae2
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 17:02:29 +0530

    Computing Operation4

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -8,3 +8,4 @@ read -p "Enter Third value: " input3
 operation1=$(( input1 + input2 * input3 ))
 operation2=$(( input1 * input2 + input3 ))
 operation3=$(( input3 + input1 / input2 ))
+operation4=$(( input1 % input2 + input3 ))
